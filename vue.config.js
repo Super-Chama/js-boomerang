a90f74c4035e33b598b4021bd22a7944bf083a70
Chamara Abesinghe <chamara@emageia.com>
2021-01-24 17:05:10 +0530

    public path added for gh-pages

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = () => {
   return {
+    publicPath: process.env.NODE_ENV === 'production' ? '/js-boomerang/' : '/',
     lintOnSave: false,
     css: {
       loaderOptions: {
